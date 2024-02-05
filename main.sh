0d740ce4e0d8aa0bfc03fd57e2d19b521f8429ef
Ari Herman <ari.j.herman@gmail.com>
2024-02-05 09:13:17 -0800

    wip

--- main.sh
+++ main.sh
@@ -4,6 +4,8 @@ learning_rates=(0.5 1.0 2.0 4.0 8.0 16.0)
 betas=(0.1 0.2 0.4 0.8 1.6 3.2 6.4 12.8)
 for beta in "${betas[@]}"; do
 for lr in "${learning_rates[@]}"; do
-        python -u main.py --learning-rate $lr --beta $beta --output-size 20 --batch-dim 20 --init zeros --n-iters 40000 > results.txt
+        name="lr_${lr}_beta_${beta}"
+        srun -N 1 -n 1 -c $cores -o $name.out --open-mode=append ./main_wrapper.sh  --learning-rate $lr --beta $beta --output-size 20 --batch-dim 20 --init zeros --n-iters 40000 > results.txt
+        # python -u main.py --learning-rate $lr --beta $beta --output-size 20 --batch-dim 20 --init zeros --n-iters 40000 > results.txt
     done
 done
