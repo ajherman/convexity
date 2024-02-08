f3da2f03115659314921cfc970dd3a97f1f58857
Ari Herman <ari.j.herman@gmail.com>
2024-02-08 14:51:21 -0700

    wip

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=main
 #SBATCH --time 10:00:00
-#SBATCH -N 10
+#SBATCH -N 12
 #SBATCH -p shared-gpu
 #module load miniconda3
 #source activate /vast/home/ajherman/miniconda3/envs/pytorch
